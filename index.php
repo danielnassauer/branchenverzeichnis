4da112e5ee6a6687726f858ae15e27dd7e3e3279
danielnassauer <daniel.nassauer@mni.thm.de>
2015-09-13 01:22:22 +0200

    Theme erzeugt

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<?php
-/*
-Plugin Name: Branchenverzeichnis
-Plugin URI: 
-Description: Branchenverzeichnis
-Version: 0.0.0
-Author: Daniel Nassauer
-License: MIT
-*/
+<html>
+<head>
 
-
-?>
+</head>
+<body>
+	<h1>Branchenverzeichnis</h1>
+</body>
+</html>
