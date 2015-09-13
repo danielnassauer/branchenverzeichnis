9719bd9d590998c4a43fa2a32dc9d819339c3bf0
danielnassauer <daniel.nassauer@mni.thm.de>
2015-09-13 15:03:11 +0200

    company login seite + template

---
+++ company_login_tmpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Company Login
+ **/
+?>
+
+<?php get_header(); ?>
+<div class="container-fluid">
+    <div class="row">
+        <?php get_sidebar(); ?>        
+        <div class="col-md-10">
+            <h2>UNTERNEHMERLOGIN</h2>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
