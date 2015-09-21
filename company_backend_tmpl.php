73f23918be4497e321a1f0e72e28b4737bc6ce70
danielnassauer <daniel.nassauer@mni.thm.de>
2015-09-21 22:35:51 +0200

    firmen lassen sich nun im Backend anlegen

--- company_backend_tmpl.php
+++ company_backend_tmpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Template Name: Company Backend
  * */
@@ -33,6 +32,27 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 } else {
     $authUrl = $client->createAuthUrl();
 }
+
+function add_post($title, $plz, $city, $street, $housenr) {
+    $post_id = wp_insert_post(array(
+        'post_type' => 'company',
+        'post_title' => $title,
+        'post_status' => 'publish'
+    ));
+    add_post_meta($post_id, 'bd_plz', $plz);
+    add_post_meta($post_id, 'bd_city', $city);
+    add_post_meta($post_id, 'bd_street', $street);
+    add_post_meta($post_id, 'bd_housenr', $housenr);
+}
+
+if (isset($_POST['bd_company_title'])) {
+    $title = $_POST['bd_company_title'];
+    $plz = $_POST['bd_plz'];
+    $city = $_POST['bd_city'];
+    $street = $_POST['bd_street'];
+    $housenr = $_POST['bd_housenr'];
+    add_post($title, $plz, $city, $street, $housenr);
+}
 ?>
 
 <?php get_header(); ?>
@@ -43,6 +63,44 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
             <h2>UNTERNEHMERBACKEND</h2>
             Angemeldet als: <?php echo $user['displayName'] ?><br>
             ID: <?php echo $user['id'] ?>
+
+            <form action="<?php the_permalink(); ?>" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label for="inputCompany" class="col-sm-2 control-label">Firma</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputCompany" placeholder="Firma" name="bd_company_title">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPLZ" class="col-sm-2 control-label">PLZ</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputPLZ" placeholder="PLZ" name="bd_plz">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputCity" class="col-sm-2 control-label">Ort</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputCity" placeholder="Ort" name="bd_city">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputStreet" class="col-sm-2 control-label">Straße</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputStreet" placeholder="Straße" name="bd_street">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputHouseNr" class="col-sm-2 control-label">Hausnummer</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputHouseNr" placeholder="Hausnummer" name="bd_housenr">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default">Firma eintragen</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
