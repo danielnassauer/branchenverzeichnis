64de28d2d55991959c252eee3784ffa88a73b462
danielnassauer <daniel.nassauer@mni.thm.de>
2015-10-01 19:38:03 +0200

    automatische Weiterleitung auf startseite

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<?php get_header(); ?>
-<div class="container-fluid">
-    <div class="row">
-        <?php get_sidebar(); ?>        
-        <div class="col-md-10">
-            <?php
-            //header('Location: index.php/suche');
-            //exit();
-            ?>
-        </div>
-    </div>
-</div>
-<?php get_footer(); ?>
+<?php
+
+header('Location: index.php/suche');
+exit();
+?>
