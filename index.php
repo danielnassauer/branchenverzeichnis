ff11c1b31e7b6c27d2cbc6ecdccdb1abfd82f8cd
danielnassauer <daniel.nassauer@mni.thm.de>
2015-10-01 19:14:44 +0200

    kleine Verbesserungen

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<?php get_header(); ?>
-<div class="container-fluid">
-    <div class="row">
-        <?php get_sidebar(); ?>        
-        <div class="col-md-10">
-            <h2>Branchenverzeichnis</h2>
-        </div>
-    </div>
-</div>
-<?php get_footer(); ?>
+<?php
+header('Location: index.php/suche');
+exit();
+?>
